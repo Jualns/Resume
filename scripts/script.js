870c488a66ce66cf1a4dfdfdd20f73e98fa348b9
João Victor da Silva <josilva602@gmail.com>
2025-01-26 18:46:53 -0300

    Add progress bar, first JS

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {
+    updateProgressBar();
+};
+
+function updateProgressBar() {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const progress = (scrollTop / scrollHeight) * 100;
+    
+    document.getElementById("progress-bar").style.width = progress + "%";
+}
